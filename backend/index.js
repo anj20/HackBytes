19916b0421e9c4f8365bb291eabdf184ddf19764
Anurag Narayan Jena <anj20@Anj-MacBook-Air.local>
2023-06-02 12:38:27 +0530

    final vercel

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,27 @@
-const express = require('express')
-const cors = require('cors');
-const { db } = require('./db/db');
-const {readdirSync} = require('fs')
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const { db } = require("./src/db/db");
+const { readdirSync } = require("fs");
+const app = express();
 
-require('dotenv').config()
+require("dotenv").config();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 //middlewares
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 //routes
-readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
+readdirSync("./src/routes").map((route) =>
+  app.use("/api/v1", require("./src/routes/" + route))
+);
 
 const server = () => {
-    db()
-    app.listen(PORT, () => {
-        console.log('listening to port:', PORT)
-    })
-}
+  db();
+  app.listen(PORT, () => {
+    console.log("listening to port:", PORT);
+  });
+};
 
-server()
+server();
