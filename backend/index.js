8f783658958743542d593f413124de65e8a3214c
Anurag Narayan Jena <96625401+anj20@users.noreply.github.com>
2023-06-27 07:03:07 +0530

    Modified Path

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { db } = require("./src/db/db");
+const { db } = require("./db/db");
 const app = express();
 const routes = require("./routes/transactions");
 
