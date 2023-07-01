b3f0d8450b3fb119717547574cc570311a719f3f
Anurag Narayan Jena <anj20@Anj-MacBook-Air.local>
2023-07-01 17:26:42 +0530

    Updated Minor Bug

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -6,7 +6,8 @@ const apiEndPoint = process.env.REACT_APP_BASE_URL;
 
 export const GlobalProvider = ({ children }) => {
   const [incomes, setIncomes] = useState([]);
-  const [expenses, setExpenses] = useState([])  const [error, setError] = useState(null);
+  const [expenses, setExpenses] = useState([]);
+  const [error, setError] = useState(null);
 
   //calculate incomes
   const addIncome = async (income) => {
